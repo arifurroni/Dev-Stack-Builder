a082aff7ef54fa92fd46511ca0b56f45ff9c23eb
Md. Arifur Rahman <arifurroni@gmail.com>
2026-09-11 12:08:01 +0600

    Hamburger button added and Navbar updated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,23 @@
 
 import logo from '../assets/logo-text.png'
+import hamburger from '../assets/hamburger.png'
 
 const Nav = () => {
     return (
         <nav className=''>
-            <div className='flex justify-between items-center mx-[10%] my-3'>
+            <div className='flex md:hidden justify-between items-center max-w-7xl mx-auto px-5 lg:px-10 my-3'>
+                <div>
+                    <a href="#menu"><img src={hamburger} alt="Hamburger Menu" /></a>
+                </div>
+                <img src={logo} alt="logo and text- Dev Stack" className='hover:cursor-pointer' />
+                    
+                <div className='flex justify-between items-center gap-5'>
+                    <button className='py-2 px-4 cursor-pointer hover:bg-pink-100 hover:rounded-4xl font-bold'>Sign In</button>
+                    <button className='bg-[#D91B7E] text-white rounded-4xl py-2 px-4 cursor-pointer font-bold hover:bg-[#fa0483]'>Sign Up</button>
+                </div>
+            </div>
+
+            <div className='hidden md:flex justify-between items-center max-w-7xl mx-auto px-5 lg:px-10 my-3'>
                 <img src={logo} alt="logo and text- Dev Stack" className='hover:cursor-pointer' />
 
                 <ul className='flex justify-between items-center gap-5'>
@@ -16,8 +29,8 @@ const Nav = () => {
                 </ul>
                     
                 <div className='flex justify-between items-center gap-5'>
-                    <button className='py-2 px-4 cursor-pointer hover:bg-pink-50 hover:rounded-4xl font-bold'>Sign In</button>
-                    <button className='bg-[#D91B7E] text-white rounded-4xl py-2 px-4 cursor-pointer font-bold'>Sign Up</button>
+                    <button className='py-2 px-4 cursor-pointer hover:bg-pink-100 hover:rounded-4xl font-bold'>Sign In</button>
+                    <button className='bg-[#D91B7E] text-white rounded-4xl py-2 px-4 cursor-pointer font-bold hover:bg-[#fa0483]'>Sign Up</button>
                 </div>
             </div>
         </nav>
